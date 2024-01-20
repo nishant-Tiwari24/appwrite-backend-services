ac88d5868dda4740ee10b69d7255917054fb17c1
its-nishant320 <124623023+its-nishant320@users.noreply.github.com>
2024-01-20 17:18:58 +0530

    store setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
+import { useEffect, useState } from 'react'
 import './App.css'
+import { useDispatch } from 'react-redux';
+import authService from './appwrite/auth';
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL)
-  return (
-    <>
-  <h1>Hey There</h1>
-    </>
-  )
+const [loading, setLoading] = useState(true);
+const dispatch = useDispatch();
+
+useEffect(()=>{
+  authService.getCurrentUser()
+  .then((userData) => {
+    if(userData) {
+      dispatch(login({userData}))
+    }
+    else {
+      dispatch(logout())
+    }
+  })
+  .finally(() => setLoading(false))
+},[])
+
+return loading ? (
+  <div>test</div>
+) : null
 }
 
 export default App
