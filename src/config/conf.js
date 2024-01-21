479b7ea28370208b591620247abee9e95aa15bfd
its-nishant320 <124623023+its-nishant320@users.noreply.github.com>
2024-01-21 16:00:23 +0530

    debugging

--- src/config/conf.js
+++ src/config/conf.js
@@ -1,7 +1,10 @@
-export const confiq = () => {
-    appwriteUrl: String(import.meta.env.VITE_APPWRITE_URL);
-    appwriteProjectID: String(import.meta.env.VITE_APPWRITE_PROJECT_ID);
-    appwriteDatabaseID: String(import.meta.env.VITE_APPWRITE_DATABASE_ID);
-    appwriteCollectionID: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID);
-    appwriteBucketID: String(import.meta.env.VITE_APPWRITE_BUCKET_ID);
+const conf =  {
+    appwriteUrl: String(import.meta.env.VITE_APPWRITE_URL),
+    appwriteProjectID: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+    appwriteDatabaseID: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
+    appwriteCollectionID: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
+    appwriteBucketID: String(import.meta.env.VITE_APPWRITE_BUCKET_ID)
+
 }
+
+export default conf;
